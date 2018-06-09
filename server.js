f7fbd2d5338619a8ad1d61858f11a9609bd376d2
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-09 18:48:53 +0900

    heroku 14

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // app.use(forceSSL());
 
-app.use(express.static(__dirname, 'dist'));
+app.use(express.static(__dirname + 'dist'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
