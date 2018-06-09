f13bb3a542942d8bf2a03d93d11f7e76ec805cd7
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-09 16:59:29 +0900

    deploy 8

--- src/server/server.js
+++ src/server/server.js
@@ -16,11 +16,11 @@ const forceSSL = function() {
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + './client/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.use(forceSSL());
 
-app.use(express.static(__dirname + './client/dist'));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
