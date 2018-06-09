a37de73649090617c47879b961f1ad157a59346e
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-09 18:18:29 +0900

    heroku 12

--- server.js
+++ server.js
@@ -13,14 +13,12 @@ const forceSSL = function() {
   }
 }
 
-// For all GET requests, send back index.html
-// so that PathLocationStrategy can be used
+app.use(express.static(__dirname, 'dist'));
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.use(forceSSL());
-
-app.use(express.static(__dirname + 'dist'));
 
 app.listen(process.env.PORT || 8080);
