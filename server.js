8b7f9f6f58d296342e5efaabe243bbd7ce942813
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-09 20:42:28 +0900

    heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.static(__dirname + 'dist'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname + 'dist/pw-keychain/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
