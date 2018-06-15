2be34296e6cb94d62fd07b76389acc24165176fe
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-15 13:45:41 +0900

    logout fix

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from "@angular/core";
 import { Router } from '@angular/router';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 export class AuthService{
 
-  constructor(public router: Router){}
+  constructor(public router: Router,
+              private http: Http){}
 
   isAuthenticated(){
     return window.localStorage.getItem('tokens') ? true : false;
   }
 
   logOut(){
-    window.localStorage.removeItem('tokens');
-    this.router.navigate(['login']);
+    const token = JSON.parse(window.localStorage.getItem('tokens')[0]);
+    this.http.delete('https://dry-stream-69567.herokuapp.com/users/me/token', {headers: new Headers({'x-auth': token})})
+      .subscribe(
+        (response) => console.log(response),
+        (error) => console.log(error),
+        () => {
+          window.localStorage.removeItem('tokens');
+          this.router.navigate(['login']);
+        }
+      );
   }
 }
